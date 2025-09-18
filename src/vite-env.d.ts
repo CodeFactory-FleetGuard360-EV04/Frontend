26bc454f77da76e437e8dea230214430e87e673f
Joselito17821 <josemanuelmany04@gmail.com>
2025-09-18 01:19:21 -0500

    Implement login functionality and improve form structure in LoginPage

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL?: string;
+    // agrega aquí otras variables VITE\_*
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
